a490bfcd55db4c493ae725067e8d9b67f678fd30
Randy Rivera <rivera.na2015@gmail.com>
2022-09-26 14:26:35 -0400

    created "Hello World" program

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log("Hello  World");
+
+a = 123;
+b = 456;
+
+console.log(a + b);
+console.log(a * 2);
+console.log(a * b);
+
+var message = "Welcome to JavaScript";
+var my_name = "Randy";
+var age = 25;
+var outputString = "Name: " + my_name + " Age: " + age;
+console.log(outputString);
+
+var myCity = "Atlanta";
+var myState = "Georgia";
+var myName = "Randy";
+
+console.log("Hello my name is " + myName + " and I'm from " + myCity + ", " + myState + ".");
