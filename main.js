14bbde720d21d8a3c547f0ed75e0da489f6f88cc
Randy Rivera <rivera.na2015@gmail.com>
2022-09-26 14:55:35 -0400

    added some exercises from workbook

--- main.js
+++ main.js
@@ -13,8 +13,29 @@ var age = 25;
 var outputString = "Name: " + my_name + " Age: " + age;
 console.log(outputString);
 
+// Exercise 1-38
+var customerId = 5432;
+var firstName = "Randy";
+var middleName = "A";
+var lastName = "Rivera";
+var gender = "Male";
+var birthDay = "1997-27-04";
+var licenseNum = "31355234";
+var policyNum = "APG123523";
+
+// Cant use while, byte, float, if, return, throws, typeof, function, boolean, import, interface etc.
+// Built in JavaScript, objects, properties words you shouldnt use are Array, hasOwnProperty, name, string, Number, eval, function, toString, length. 
+// HTML and window objects/ properties to avoid using are area, assign, event, images, status, submit, link, image, innerWidth, text, button, anchors etc.
+// HTML event names you shouldnt use are, onblur, onclick, onerror, onload, onkeypress, onmouseup, onmousedown.
+
 var myCity = "Atlanta";
 var myState = "Georgia";
 var myName = "Randy";
 
 console.log("Hello my name is " + myName + " and I'm from " + myCity + ", " + myState + ".");
+
+var num1 = 15;
+var answer;
+
+answer = num1 ** 2; // answer is 225, another way to raise a number to a power is Math.pow()
+console.log(answer);
